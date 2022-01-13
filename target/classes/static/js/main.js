508a451eb53d642ca59d7d31e0d0abadf7e55d35
WiktorKarnia <wiktor1341@o2.pl>
2022-01-13 18:36:15 +0100

    13.01.2022

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -4,9 +4,88 @@ const getProducts = () => {
         .catch(error => console.log(error));
 }
 
+const createHtmlElementFromString = (htmlAsString) => {
+    let myElement = document.createElement('div');
+    myElement.innerHTML = htmlAsString.trim();
+
+    return myElement.firstChild;
+}
+
+const createProductComponent = (product) => {
+    const template = `
+        <li class="product">
+            <h3>${product.title}</h3>
+            <div class="product__image">
+                <img src="${product.mediaPath}" />
+            </div>
+            <span class="product__price">${product.price}</span>
+            <button
+                class="product__add-to-basket"
+                data-product-id="${product.id}"
+            >Add to basket</button>
+        </li>
+    `;
+
+    return createHtmlElementFromString(template);
+}
+
+const appendToProductList = (htmlList, htmlElements) => {
+    htmlElements.forEach(element => htmlList.appendChild(element));
+
+    return htmlElements;
+}
+
+const handleAddToBasket = (productId) => {
+    const url = `/api/add-product/${productId}`;
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    });
+}
+
+const updateBasketComponent = (offer) => {
+    const basketEl = document.querySelector('.basket');
+    basketEl.querySelector('.basket__items-count').innerText = `${offer.linesCount} Items`;
+    basketEl.querySelector('.basket__total').innerText = `${offer.total} PLN`;
+}
+
+const refreshCurrentOffer = () => {
+    return fetch('/api/current-offer')
+        .then(r => r.json())
+        .then(offer => updateBasketComponent(offer))
+}
+
+const initializeAddToBasketHandler = (htmlElements) => {
+    htmlElements.forEach(element => {
+        const addToBasketBtn = element.querySelector('.product__add-to-basket');
+
+        addToBasketBtn.addEventListener('click', (event) => {
+            const productId = event.target.getAttribute('data-product-id');
+            handleAddToBasket(productId)
+                .then(() => refreshCurrentOffer())
+                .catch((err) => console.log('something is not YES :>'))
+        });
+    })
+    return htmlElements;
+}
+
 (() => {
-    console.log("It works :) :)");
+    console.log("It works :):)");
+    const productsList = document.querySelector('.products__list');
+    const buyBtn = document.getElementById('buy');
+
+    buyBtn.addEventListener('click', () => {
+        console.log('accept offer needs to be implemented');
+    })
+
+    refreshCurrentOffer()
+        .then(() => console.log('offer should be refreshed'));
 
     getProducts()
-    .then(products => console.log(products))
+        .then(productsAsJsonObject => productsAsJsonObject.map(createProductComponent))
+        .then(productsAsHtmlEl => initializeAddToBasketHandler(productsAsHtmlEl))
+        .then(productsAsHtmlEl => appendToProductList(productsList, productsAsHtmlEl))
+
 })();
